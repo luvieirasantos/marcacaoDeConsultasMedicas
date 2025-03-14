a1e991fec6460cb9748f41bc6908bca7f611b323
luis <henrique3.terceiro@gmail.com>
2025-03-13 22:25:17 -0300

    Header melhorada

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,21 @@ import { StatusBar } from 'react-native';
 
 export const HeaderContainer = styled.View`
   background-color: #007BFF;
-  padding-top: ${StatusBar.currentHeight}px;
-  padding: 20px;
+  padding-top: ${StatusBar.currentHeight ? StatusBar.currentHeight + 10 : 40}px;
+  padding-bottom: 15px;
+  align-items: center;
+  justify-content: center;
+  elevation: 4;
+  shadow-color: #000;
+  shadow-offset: 0px 2px;
+  shadow-opacity: 0.2;
+  shadow-radius: 3px;
 `;
 
 export const HeaderTitle = styled.Text`
   color: white;
-  font-size: 20px;
+  font-size: 22px;
   font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 1px;
 `;
